ac0a87c8282f008ac7af2e30da2322e596b0f632
Joshua Ibrom (sk3p7ic) <joshuaibrom@gmail.com>
2023-09-03 16:08:00 -0500

    Worked on basic styling.
    Melange colorscheme is now used.

---
+++ src/components/MenuBar.svelte
@@ -0,0 +1,9 @@
+<div id="menu-bar">
+	<p>Test</p>
+</div>
+
+<style>
+	div#menu-bar {
+		background-color: var(--black);
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<script lang="ts">
+	import MenuBar from '../components/MenuBar.svelte';
+</script>
+
+<MenuBar />
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+</style>
